c743e4ee3abee8da427a6b59312de29e695b261b
Ryan Romanchuk <rromanchuk@gmail.com>
2017-07-10 22:23:47 -0700

    coerce int

--- app/models/node.rb
+++ app/models/node.rb
@@ -3,6 +3,6 @@ class Node < ApplicationRecord
   update_index('nodes#node') { self }
 
   def self.height
-    Redis.current.get('height') || 0
+    Redis.current.get('height').to_i || 0
   end
 end
