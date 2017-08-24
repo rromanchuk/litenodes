322672cd46fdb95f7a1e1cf48012ab66a60c5b3a
Ryan Romanchuk <rromanchuk@gmail.com>
2017-08-24 10:43:59 -0700

    use recent snapshot

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
   def index
+    @nodes = Snapshot.recent_nodes
     @host_address = request.remote_ip
-    @countries = Node.group(:country).order('count_all desc').limit(10).count
-    @all_nodes = Node.count
+    @countries = @nodes.group(:country).order('count_all desc').limit(10).count
+    @all_nodes = @nodes.count
   end
 end
