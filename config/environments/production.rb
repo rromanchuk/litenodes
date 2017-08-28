9ed51caaee2d1535c80f0c5c79812ce941343105
Ryan Romanchuk <rromanchuk@gmail.com>
2017-08-27 19:25:59 -0700

    use cf distro

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.action_controller.asset_host = 'cdn.litenodes.net'
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
