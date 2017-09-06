5dd21aa9a3815f1d87564829a59514efe59e3267
Ryan Romanchuk <rromanchuk@gmail.com>
2017-09-05 23:55:54 -0600

    fix order

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,7 @@ class SearchController < ApplicationController
     @nodes = []
     if params[:q]
       @q = sanitize_string(params[:q])
-      @nodes = Node.search(@q).order(:timestamp).page(params[:page]).objects
+      @nodes = Node.search(@q).order(timestamp: {order: :desc}).page(params[:page]).objects
     end
   end
 
